e2077c089f53bab065293872686ef2cd9e16d164
ravel <sebastien.ravel@cirad.fr>
2020-07-30 16:31:01 +0200

    fix bug

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -28,7 +28,8 @@ PATH = os.path.abspath('../../yoda_powers/scripts/')
 
 if os.environ.get('READTHEDOCS') == 'True':
     PATH = os.path.abspath('./yoda_powers/scripts/')
-    sys.path.insert(0, os.path.abspath('..yoda_powers/scripts/'))
+    sys.path.insert(0, os.path.abspath('../../yoda_powers/scripts/'))
+    sys.path.insert(0, os.path.abspath('./yoda_powers/scripts/'))
 
 # -- General configuration ------------------------------------------------
 
