7abc5ff16c9c25128c319bfd66df21f012b80cc1
ravel <sebastien.ravel@cirad.fr>
2020-07-31 10:36:49 +0200

    fix bug

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -19,6 +19,7 @@ import os
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
+sys.path.insert(0, os.path.abspath('./../..'))
 sys.path.insert(0, os.path.abspath('./'))
 sys.path.insert(0, os.path.abspath('../../'))
 sys.path.insert(0, os.path.abspath('../../yoda_powers/'))
@@ -27,9 +28,9 @@ sys.path.insert(0, os.path.abspath('../../yoda_powers/scripts/'))
 PATH = os.path.abspath('../../yoda_powers/scripts/')
 
 if os.environ.get('READTHEDOCS') == 'True':
-    PATH = os.path.abspath('./yoda_powers/scripts/')
-    sys.path.insert(0, os.path.abspath('../../yoda_powers/scripts/'))
-    sys.path.insert(0, os.path.abspath('./yoda_powers/scripts/'))
+    PATH = os.path.abspath('./../yoda_powers/scripts/')
+    sys.path.insert(0, os.path.abspath('./../yoda_powers/scripts/'))
+    sys.path.insert(0, os.path.abspath('./../..'))
 
 # -- General configuration ------------------------------------------------
 
