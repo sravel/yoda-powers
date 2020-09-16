f1548e62383954596d0b99053a624753384ff606
ravel <sebastien.ravel@cirad.fr>
2020-09-16 16:21:09 +0200

    add decorator welcome_args

---
+++ docs/source/api/yoda_powers.toolbox.welcome_args.rst
@@ -0,0 +1,6 @@
+welcome_args
+============
+
+.. currentmodule:: yoda_powers.toolbox
+
+.. autofunction:: welcome_args
--- yoda_powers/toolbox/toolbox.py
+++ yoda_powers/toolbox/toolbox.py
@@ -5,11 +5,73 @@
 # Modules
 ##################################################
 # Python modules
-from pathlib import PosixPath
+from pathlib import PosixPath, Path
+from datetime import datetime
+import sys
 
 
 ##################################################
 # Functions
+
+def welcome_args(version_arg, parser_arg):
+    """
+    use this Decorator to add information to scripts with arguments
+
+    Args:
+        version_arg: the program version
+        parser_arg: the function which return :class:`argparse.ArgumentParser`
+
+    Returns:
+        None:
+
+    Notes:
+        use at main() decorator for script with :class:`argparse.ArgumentParser`
+
+    Examples:
+        >>> from yoda_powers.toolbox import welcome_args
+        >>> @welcome_args(version, build_parser())
+        >>> def main():
+        >>>     # some code
+        >>> main()
+        >>> ################################################################################
+        >>> #                             prog_name and version                            #
+        >>> ################################################################################
+        >>> Start time: 16-09-2020 at 14:39:02
+        >>> Commande line run: ./filter_mummer.py -l mummer/GUY0011.pp1.fasta.PH0014.pp1.fasta.mum
+        >>>
+        >>> - Intput Info:
+        >>>         - debug: False
+        >>>         - plot: False
+        >>>         - scaff_min: 1000000
+        >>>         - fragments_min: 5000
+        >>>         - csv_file: blabla
+        >>> PROGRAMME CODE HERE
+        >>> Stop time: 16-09-2020 at 14:39:02       Run time: 0:00:00.139732
+        >>> ################################################################################
+        >>> #                               End of execution                               #
+        >>> ################################################################################
+
+    """
+    def welcome(func):
+        def wrapper():
+            start_time = datetime.now()
+            parser = parser_arg
+            version = version_arg
+            parse_args = parser.parse_args()
+            # Welcome message
+            print(
+                    f"""{"#" * 80}\n#{Path(parser.prog).stem + " " + version:^78}#\n{"#" * 80}\nStart time: {start_time:%d-%m-%Y at %H:%M:%S}\nCommande line run: {" ".join(sys.argv)}\n""")
+            # resume to user
+            print(" - Intput Info:")
+            for k, v in vars(parse_args).items():
+                print(f"\t - {k}: {v}")
+            print("\n")
+            func()
+            print(
+                    f"""\nStop time: {datetime.now():%d-%m-%Y at %H:%M:%S}\tRun time: {datetime.now() - start_time}\n{"#" * 80}\n#{'End of execution':^78}#\n{"#" * 80}""")
+        return wrapper
+    return welcome
+
 
 def compare_list(list1, list2):
     """
